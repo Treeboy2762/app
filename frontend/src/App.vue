256599f8d4ee7cd2e9709818d4c2dad1f99b630f
Dan Kim <kkds7403@naver.com>
2020-08-18 03:35:59 +0900

    Frontend
    updata

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-      <form @submit.prevent="createMovie">
+      <form @submit.prevent="submitForm">
         <div class="form-group row">
           <input type="text" class="form-control col-3 mx-2" placeholder="id" v-model="movie.id">
           <input type="text" class="form-control col-3 mx-2" placeholder="title" v-model="movie.title">
@@ -17,13 +17,24 @@
           <th>Genre</th>
         </thead>
         <tbody>
-          <tr v-for="movie in movies" :key="movie.id">
+          <tr v-for="movie in movies" :key="movie.id" @dblclick="$data.movie = movie">
             <td>{{ movie.id }}</td>
             <td>{{ movie.title }}</td>
             <td>{{ movie.genre }}</td>
+            <td>
+              <button class="btn btn-outline-danger btn-sm ms-1" @click="deleteMovie(movie)">x</button>
+            </td>
           </tr>
         </tbody>
       </table>
+    <div v-show="is_show">
+        <h5>뷰하!</h5>
+        <p>v-if와 v-show로 모달창을 띄워봅시다.</p>
+        <p>{{ placeholder }}</p>
+        <button @click="handle_toggle" type="button">
+            확인
+        </button>
+    </div>
   </div>
 </template>
 
@@ -33,30 +44,78 @@ export default {
   name: 'App',
   data(){
     return {
-      movie: {
-        'id': '',
-        'title': '',
-        'genre': '',
-      },
-      movies: []
+      movie: {},
+      movies: [],
+      is_show: false,
+      placeholder: '',
     }
   },
 
   async created(){
-    var response = await fetch('http://localhost:8000/api/movies/');
-      this.movies = await response.json();
+    await this.getMovies();
   },
 
   methods: {
-    async createStudent(){
-      var response = await fetch('http://localhost:8000/api/movies/', {
+
+    handle_toggle: function(text){ 
+      this.is_show = !this.is_show; // #2, #3
+      this.placeholder = text;
+    },
+
+    submitForm(){
+      if (this.movie.id === undefined) {
+        this.createMovie();
+      }
+
+      else {
+        this.editMovie();
+      }
+        
+    },
+
+    async getMovies() {
+      var response = await fetch('http://localhost:8000/api/movies/');
+      this.movies = await response.json();
+    },
+
+    async createMovie(){
+      await this.getMovies
+
+      await fetch('http://localhost:8000/api/movies/', {
         method: 'post',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(this.movie)
       });
-      this.movies.push(await response.json());
+      await this.getMovies();
+    },
+
+    async editMovie(){
+      await this.getMovies
+
+      await fetch(`http://localhost:8000/api/movies/${this.movie.id}/`, {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.movie)
+      });
+      await this.getMovies();
+      this.movie = {}
+    },
+
+    async deleteMovie(movie){
+      await this.getMovies
+
+      await fetch(`http://localhost:8000/api/movies/${movie.id}/`, {
+        method: 'delete',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.movie)
+      });
+      await this.getMovies();
     }
   }
 }
